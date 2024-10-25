cbf3ca81ffd814d17ff8e602fed93acd32752ebc
robiulislam485 <jibonmahmudrobi485@gmail.com>
2024-10-25 18:42:12 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className='text-2xl font-bold'>Select your 11</h1>
+     <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Im from header</h1>
+        </div>
+    );
+};
+
+export default Header;
